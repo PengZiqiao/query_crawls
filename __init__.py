4171c91d93c13ac009b234d08ca72765addc59f3
PengZiqiao <xfyx.student@sina.com>
2018-05-09 10:55:03 +0800

    增加土地查询

--- __init__.py
+++ __init__.py
@@ -10,16 +10,16 @@ import pandas as pd
 
 
 class CricMarketOption:
-    city = ''
+    city = '南京'
     trade_source = '1类'
     time_type = 'Month'  # Day, Week, Month, Quater, Year
-    time = ''  # 2018-01-01, 2018年01周, 2018年01月, 2018年03季度, 2018年, 开始、结束用冒号分隔
+    time = '2018年01月:2018年05月'  # 2018-01-01, 2018年01周, 2018年01月, 2018年03季度, 2018年, 开始、结束用冒号分隔
     region_type = 'Region'  # Region:区域, District:板块, Circle:环线
     region = ''  # 多个值用逗号分隔
     usage = '普通住宅,别墅'  # 普通住宅, 别墅, 酒店式公寓, 商业, 办公, 多个值用逗号分隔
     rows = 'time'  # time, region, RoomUsage, Step.AreaStep, Step.PriceStep, Step.AllMoneyStep
     columns = []  # 同rows
-    outputs = ''  # 供应面积 供应套数 成交面积 成交套数 成交均价 套均价 成交金额 供求比 环比 同比
+    outputs = '供应面积,成交面积,成交均价'  # 供应面积 供应套数 成交面积 成交套数 成交均价 套均价 成交金额 供求比 环比 同比
     rowtotal = False
 
     @property
@@ -64,10 +64,58 @@ class CricMarketOption:
 
         # 返回一个嵌套字典，作为post传递的数据
         return {
-            'paramStr': json.dumps(params),
+            'paramStr': json.dumps(params, ensure_ascii=False),
             'cityName': self.city,
             'tableConfig': 'Date',
             'MenuKey': 100010
+        }
+
+
+class CricLandOption:
+    by = 'time'  # time, city,
+    province = '江苏省'
+    city = '南京'
+    region = ''
+    district = ''
+    date_type = '月度'  # 月度, 季度, 年度
+    date = '2018年01月:2018年05月'
+    state = '成交'  # 成交, 供应
+    method = '招拍挂土地'  # 协议土地, 招拍挂土地, 其他出让土地
+    usage = '纯住宅,商住'  # 纯住宅, 商住, 商办, 综合用地, 工业, 其他
+
+    @property
+    def data(self):
+        # 基本筛选项
+        data = [['PROVINCE', 'PROVINCE', self.province],
+                ['CITY_NAME', 'CITY_NAME', self.city],
+                ['BIDDING_DATE', self.date_type, ''],
+                ['LAND_TRADE_STATE', '', self.state],
+                ['BIDDING_METHOD', '', self.method],
+                ['USE_TYPE_TAG', '', self.usage]]
+
+        # 区域和板块筛选
+        if self.region:
+            data.append(['REGION', 'REGION', self.region])
+        if self.district:
+            data.append(['DISTRICT', 'DISTRICT', self.district])
+
+        # 生成
+        df = pd.DataFrame(data, columns=['Key', 'Tag', 'Value'])
+
+        # 日期筛选
+        start, end = self.date.split(':')
+        df['MinValue'], df['MaxValue'], df['Type'] = '', '', ''
+        df.loc[df.Key == 'BIDDING_DATE', ['MinValue', 'MaxValue', 'Type']] = start, end, 'Date'
+
+        # 返回一个嵌套字典，作为post传递的数据
+        by = {'time': ('按时间', 200002), 'city': ('按城市', '200003')}
+        return {
+            'strParam': json.dumps(df.to_dict('record'), ensure_ascii=False),
+            'tablestyle': '交叉模式',
+            'tablefield': 'PROPERTY',
+            'statistictype': by[self.by][0],
+            'statisticdetailtitle': f'{start}至{end}土地{self.state}明细',
+            'MenuKey': by[self.by][1]
         }
 
 
@@ -101,10 +149,16 @@ class Cric:
         self._get_cookie()
         self._login()
 
+    def _post(self, url, data):
+        return self.s.post(url, data).text
+
     def market(self, data):
-        url = 'https://app.cric.com/Statistic/MarketMonitor/GetComprehensiveTableData'
-        html = self.s.post(url, data).text
+        html = self._post('https://app.cric.com/Statistic/MarketMonitor/GetComprehensiveTableData', data)
         return self._html2df(html)
+
+    def land(self, data):
+        html = self._post('https://app.cric.com/land/Statistic/Statistic/StatisticTable', data)
+        return pd.read_html(html, index_col=0, header=0)[0]
 
     @staticmethod
     def _html2df(html):
